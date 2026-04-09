80947f5075e05947b476231447bd6fdbe178cb10
student <student@kbkit.pri>
2026-04-09 15:03:55 +0200

    hg

--- Homepage/script.js
+++ Homepage/script.js
@@ -1,23 +1,24 @@
 const users = [
     {imglogo: "./assets/eat1.svg", name: "Spaghetti", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
-    {imglogo: "./assets/eat1.svg", name: "Spaghetti", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
-    {imglogo: "./assets/eat1.svg", name: "Spaghetti", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
+    {imglogo: "./assets/eat2.svg", name: "Gnocchi", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
+    {imglogo: "./assets/eat3.svg", name: "Rovioli", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
 ];
 
 
  const usersDiv = document.getElementById('menu_card_one_rows');
 
  usersDiv.innerHTML = users.map(
-    item => `<div class = "menu_card">
-                <img src="${item.imglogo}">
-                <div class = "menu_block1">
-                    <p>${item.name}</p> 
-                    <img src="${item.imgrait}">
-                <div>
-                <p>${item.opis}</p>
-                <div>
-                    <p>${item.sell}</p>
-                    <button>${item.button}
-                </div>
-             </div>`,
+    item => `
+        <div class = "menu_card" >
+            <img class = "menu_img_main" src="${item.imglogo}">
+            <div class = "menu_block1">
+                <p>${item.name}</p>
+                <img src="${item.imgrait}">
+            </div>
+            <p>${item.opis}</p>
+            <div>
+                <p>${item.sell}</p>
+                <button>${item.button}</button>
+            </div>
+         </div>`,
     ).join('');
