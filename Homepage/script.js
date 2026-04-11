1f29f6e4402ec4b14de19e83f71a308296ee6d28
Svetlana184 <sveta.kgd.39@gmail.com>
2026-04-11 11:50:20 +0200

    asd

--- Homepage/script.js
+++ Homepage/script.js
@@ -2,6 +2,10 @@ const users = [
     {imglogo: "./assets/eat1.svg", name: "Spaghetti", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
     {imglogo: "./assets/eat2.svg", name: "Gnocchi", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
     {imglogo: "./assets/eat3.svg", name: "Rovioli", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
+     {imglogo: "./assets/eat4.svg", name: "Penne Alla Vodak", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
+    {imglogo: "./assets/eat5.svg", name: "Risoto", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
+    {imglogo: "./assets/eat6.svg", name: "Splitza Signature", opis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas consequat mi eget auctor aliquam, diam.", imgrait:"./assets/rating.svg", sell: "$12.05", button: "Order now"},
+
 ];
 
 
@@ -15,10 +19,10 @@ const users = [
                 <p>${item.name}</p>
                 <img src="${item.imgrait}">
             </div>
-            <p>${item.opis}</p>
-            <div>
-                <p>${item.sell}</p>
-                <button>${item.button}</button>
+            <p class = "menu_text_nomain">${item.opis}</p>
+            <div class = "menu_sell_block">
+                <p class = "menu_sell_block_text">${item.sell}</p>
+                <button class = "menu_sell_block_button">${item.button}</button>
             </div>
          </div>`,
     ).join('');
